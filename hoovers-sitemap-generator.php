2acfd450244eec8e48e45656920f0857bdd5db20
Matthew Hoover <matthew@hoover90.net>
2014-09-25 14:43:38 -0400

    Fixed some coding standards issues.

--- hoovers-sitemap-generator.php
+++ hoovers-sitemap-generator.php
@@ -12,28 +12,37 @@
 
 
 function hoovers_sitemap_generator_activate() {
+
 	hoovers_sitemap_generator_rules();
 	flush_rewrite_rules();
+
 }
 
 
 function hoovers_sitemap_generator_deactivate() {
+
 	flush_rewrite_rules();
+
 }
 
 function hoovers_sitemap_generator_rules() {
+
 	add_rewrite_rule("sitemap/([0-9]|[0-9]+)/?$", 'index.php?pagename=sitemap&smp=$matches[1]', "top");
 	add_rewrite_rule('sitemap/static/?$', 'index.php?pagename=sitemapstatic', 'top');
 	add_rewrite_rule('sitemap/archive/?$', 'index.php?pagename=sitemaparchive', 'top');
 	add_rewrite_rule('sitemap/?$', 'index.php?pagename=sitemapindex', 'top');
+
 }
 
 function hoovers_sitemap_generator_query_vars($vars) {
+
 	$vars[] = 'smp';
 	return $vars;
+
 }
 
 function hoovers_sitemap_generator_display() {
+
 	global $wpdb, $wp_locale;
 
 	// determine which part of the sitemap we need via pagename
@@ -91,32 +100,39 @@ function hoovers_sitemap_generator_display() {
 			$modified_date = the_modified_date('Y-m-d\TH:i:s', '', '', false).
 						w3c_tz_str(get_option('gmt_offset'));
 
-
 			$pdm = get_the_time('m');
 			$pdy = get_the_time('Y');
 
 			if(hoovers_within_prev($pdy, $pdm, $months=1)) {
+
 				$priority = $post_priority_1m;
 				$changefreq = $post_cf_1m;
+
 			}
 			else if(hoovers_within_prev($pdy, $pdm, $months=3)) {
+
 				$priority = $post_priority_3m;
 				$changefreq = $post_cf_3m;
+
 			}
 			else if(hoovers_within_prev($pdy, $pdm, $months=6)) {
+
 				$priority = $post_priority_6m;
 				$changefreq = $post_cf_6m;
+
 			}
 			else if(hoovers_within_prev($pdy, $pdm, $months=12)) {
+
 				$priority = $post_priority_12m;
 				$changefreq = $post_cf_12m;
+
 			}
 			else {
+
 				$priority = $post_priority_gt12m;
 				$changefreq = $post_cf_gt12m;
+
 			}
-
-
 
 			echo " <url>\n";
 			echo "  <loc>".$permalink."</loc>\n";
@@ -127,13 +143,13 @@ function hoovers_sitemap_generator_display() {
 
 		endwhile;
 
-
 		hoovers_sitemap_footer();
 		exit();
+
 	}
 	else if($page == 'sitemapstatic') {
-		hoovers_sitemap_header();
 
+		hoovers_sitemap_header();
 
 		// pages - add the home page and other static stuffs
 		echo " <url>\n";
@@ -165,8 +181,10 @@ function hoovers_sitemap_generator_display() {
 
 		hoovers_sitemap_footer();
 		exit();
+
 	}
 	else if($page == 'sitemapindex') {
+
 		hoovers_sitemap_index_header();
 
 		$query = new WP_Query(array(
@@ -186,51 +204,61 @@ function hoovers_sitemap_generator_display() {
 		echo " </sitemap>\n";
 
 		for($i = 1; $i <= $query->max_num_pages; $i++) {
+
 			echo " <sitemap>\n";
 			echo "  <loc>".get_site_url()."/sitemap/".$i."/</loc>\n";
 			echo " </sitemap>\n";
+
 		}
 
 		hoovers_sitemap_index_footer();
 		exit();
+
 	}
 	else if($page == 'sitemaparchive') {
+
 		hoovers_sitemap_header();
 
 		$where = apply_filters( 'getarchives_where', "WHERE post_type = 'post' AND post_status = 'publish'", array() );
-
-
 		$last_changed = wp_cache_get( 'last_changed', 'posts' );
-		if ( ! $last_changed ) {
+
+		if (!$last_changed) {
+
 			$last_changed = microtime();
 			wp_cache_set( 'last_changed', $last_changed, 'posts' );
+
 		}
 
 		$query = "SELECT YEAR(post_date) AS `year`, MONTH(post_date) AS `month`, count(ID) as posts FROM $wpdb->posts $where GROUP BY YEAR(post_date), MONTH(post_date) ORDER BY post_date";
-		$key = md5( $query );
+		$key = md5($query);
 		$key = "wp_get_archives:$key:$last_changed";
 
-		if ( ! $results = wp_cache_get( $key, 'posts' ) ) {
-			$results = $wpdb->get_results( $query );
-			wp_cache_set( $key, $results, 'posts' );
+		if (!$results = wp_cache_get($key, 'posts') ) {
+
+			$results = $wpdb->get_results($query);
+			wp_cache_set($key, $results, 'posts');
+
 		}
-		if ( $results ) {
+		if ($results) {
 
-			foreach ( (array) $results as $result ) {
-				$url = get_month_link( $result->year, $result->month );
+			foreach ((array) $results as $result) {
 
+				$url = get_month_link($result->year, $result->month);
 
 				echo " <url>\n";
 				echo "  <loc>".$url."</loc>\n";
 				echo "  <changefreq>".(hoovers_within_prev($result->year, $result->month, 1) ? "daily" : (hoovers_within_prev($result->year, $result->month, 6) ? "weekly" : "monthly"))."</changefreq>\n";
 				echo "  <priority>".$monthly_priority."</priority>\n";
 				echo " </url>\n";
+
 			}
+
 		}
 
 
 		hoovers_sitemap_footer();
 		exit();
+
 	}
 
 }
@@ -244,19 +272,25 @@ function hoovers_within_prev($y, $m, $months=6, $years=0, $include_earliest=fals
 
 	// correct for excessive use of months
 	while($months >= 12) {
+
 		$earliest_year--;
 		$months = $months - 12;
+
 	}
 
 	// if we have more months left than the 
 	// current month of the year, fix it
 	if($months >= $cm) {
+
 		$earliest_year--;
 		$months = $months - $cm;
 		$earliest_month = 12 - $months;
+
 	}
 	else {
+
 		$earliest_month = $cm - $months;
+
 	}
 
 
@@ -264,18 +298,26 @@ function hoovers_within_prev($y, $m, $months=6, $years=0, $include_earliest=fals
 	// return true, return false if it is less than the earliest
 	// year
 	if($y > $earliest_year) {
+
 		return true;
+
 	}
 	else if($y < $earliest_year) {
+
 		return false;
+
 	}
 
 	// check if the given month should be included
 	if($include_earliest && $m >= $earliest_month) {
+
 		return true;
+
 	}
 	else if(!$include_earliest && $m > $earliest_month) {
+
 		return true;
+
 	}
 
 	return false;
@@ -283,19 +325,24 @@ function hoovers_within_prev($y, $m, $months=6, $years=0, $include_earliest=fals
 }
 
 function hoovers_sitemap_comment() {
+
 	// Please don't delete this; I will be sad.  :'(
 	echo " <!--                                                     -->\n";
 	echo " <!--   Sitemap generated by Hoover's Sitemap Generator   -->\n";
 	echo " <!--            http://wpsitemap.hoovism.com             -->\n";
 	echo " <!--                                                     -->\n";
+
 }
 
 function hoovers_sitemap_http_headers() {
+
 	header('HTTP/1.1 200 OK');
 	header('Content-Type: application/xml');
+
 }
 
 function hoovers_sitemap_header() {
+
 	hoovers_sitemap_http_headers();
 
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
@@ -305,45 +352,63 @@ function hoovers_sitemap_header() {
 	echo " http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n";
 
 	hoovers_sitemap_comment();
+
 }
 
 function hoovers_sitemap_index_header() {
+
 	hoovers_sitemap_http_headers();
 
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	echo "<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
 
 	hoovers_sitemap_comment();
+
 }
 
 function hoovers_sitemap_footer() {
+
 	echo "</urlset>";
+
 }
 
 function hoovers_sitemap_index_footer() {
+
 	echo "</sitemapindex>";
+
 }
 
 function w3c_tz_str($dec_offset) {
+
 	if($dec_offset < 0) {
+
 		$sign = "-";
 		$dec_offset = abs($dec_offset);
+
 	}
 	else {
+
 		$sign = "+";
+
 	}
 
 	$hours = intval($dec_offset);
 	$minutes = ($dec_offset - $hours) * 60;
+
 	return $sign.leading_zero($hours).":".leading_zero($minutes);
+
 }
 
 function leading_zero($str, $digits=2) {
+
 	while(strlen($str) < $digits) {
+
 		$str = "0".$str;
+
 	}
 
 	return $str;
+
 }
 
 
